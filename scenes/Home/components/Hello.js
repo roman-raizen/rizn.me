dc4cb7cc407c036de9d2f1cc1f42cee089a34b68
Roman Raizen <raizen.me@icloud.com>
2021-12-27 00:38:46 +0700

    Responsive Hello panel

--- scenes/Home/components/Hello.js
+++ scenes/Home/components/Hello.js
@@ -10,25 +10,27 @@ const Hello = () => {
   return (
     <Section>
       <Blobs>
-        <div className="flex flex-col-reverse p-10 bg-white rounded-lg">
-          <div className="text-center">
-            <div className="text">Hello, I'm</div>
-            <h1 className="text-4xl font-bold my-3">Roman Raizen</h1>
-            <div className="text">
-              <Highlight>
-                I create web
-              </Highlight>.
-              Very passionate about web technologies and learning new skills.
-              Recently into <Highlight>Web3</Highlight> and <Highlight>Blockchain</Highlight> technologies.
-            </div>
-            <div className="flex justify-center mt-10">
-              <InstagramIcon className="mx-4 w-10 h-10 fill-slate-600" />
-              <GithubIcon className="mx-4 w-10 h-10 fill-slate-600" />
-              <LinkedInIcon className="mx-4 w-10 h-10 fill-slate-600" />
+        <div className="flex flex-col-reverse p-10 bg-white rounded-lg lg:flex-row content-center">
+          <div className="text-center lg:w-1/2 mr-10 mt-10 justify-center flex-col flex">
+            <div>
+              <div className="text">Hello, I'm</div>
+              <h1 className="text-4xl font-bold my-3">Roman Raizen</h1>
+              <div className="text">
+                <Highlight>
+                  I create web
+                </Highlight>.
+                Very passionate about web technologies and learning new skills.
+                Recently into <Highlight>Web3</Highlight> and <Highlight>Blockchain</Highlight> technologies.
+              </div>
+              <div className="flex justify-center mt-10">
+                <InstagramIcon className="mx-4 w-10 h-10 fill-slate-600" />
+                <GithubIcon className="mx-4 w-10 h-10 fill-slate-600" />
+                <LinkedInIcon className="mx-4 w-10 h-10 fill-slate-600" />
+              </div>
             </div>
           </div>
-          <div className="flex justify-center mb-10">
-            <img className="rounded-full w-1/2" alt="Roman Raizen" src='/me.jpg' />
+          <div className="flex justify-center xs:mb-10 lg:w-1/2">
+            <img className="rounded-full w-1/2 lg:w-full" alt="Roman Raizen" src='/me.jpg' />
           </div>
         </div>
       </Blobs>
