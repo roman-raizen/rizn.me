61a56124b27965f64e2838822876da97f31df91b
Roman Raizen <raizen.me@icloud.com>
2022-01-09 13:33:10 +0700

    Fixes Blobs in Hello section

--- components/Blobs.js
+++ components/Blobs.js
@@ -1,9 +1,9 @@
-const Blobs = ({children, width = "w-4/5"}) => {
+const Blobs = ({children, className }) => {
   return (
     <div className="relative">
-      <div className={`absolute animate-blob ${width} aspect-square top-0 -left-4 bg-purple-200 rounded-full mix-blend-multiply blur-lg filter`}></div>
-      <div className={`absolute animate-blob ${width} aspect-square top-0 -right-4 bg-yellow-200 rounded-full mix-blend-multiply blur-lg filter animation-delay-4000`}></div>
-      <div className={`absolute animate-xc-blob ${width} aspect-square left-1/2 -translate-x-1/2 -bottom-4 bg-pink-200 rounded-full mix-blend-multiply blur-lg filter animation-delay-8000`}></div>
+      <div className={`absolute animate-blob w-4/5 aspect-square top-0 -left-4 bg-purple-200 rounded-full mix-blend-multiply blur-lg filter ${className}`}></div>
+      <div className={`absolute animate-blob w-4/5 aspect-square top-0 -right-4 bg-yellow-200 rounded-full mix-blend-multiply blur-lg filter animation-delay-4000 ${className}`}></div>
+      <div className={`absolute animate-xc-blob w-4/5 aspect-square left-1/2 -translate-x-1/2 -bottom-4 bg-pink-200 rounded-full mix-blend-multiply blur-lg filter animation-delay-8000 ${className}`}></div>
 
       <div className="relative">
         {children}
--- scenes/Home/components/Hello.js
+++ scenes/Home/components/Hello.js
@@ -9,12 +9,12 @@ import Section from '@/components/Section'
 const Hello = () => {
   return (
     <Section>
-      <Blobs>
-        <div className="flex flex-col-reverse p-10 bg-white rounded-lg lg:flex-row content-center">
-          <div className="text-center lg:w-1/2 mr-10 mt-10 justify-center flex-col flex">
+      <Blobs className="lg:w-1/2">
+        <div className="flex flex-col-reverse content-center p-10 bg-white rounded-lg lg:flex-row">
+          <div className="flex flex-col justify-center mt-10 mr-10 text-center lg:w-1/2">
             <div>
               <div className="text">Hello, I'm</div>
-              <h1 className="text-4xl font-bold my-3">Roman Raizen</h1>
+              <h1 className="my-3 text-4xl font-bold">Roman Raizen</h1>
               <div className="text">
                 <Highlight>
                   I create web
@@ -23,14 +23,14 @@ const Hello = () => {
                 Recently into <Highlight>Web3</Highlight> and <Highlight>Blockchain</Highlight> technologies.
               </div>
               <div className="flex justify-center mt-10">
-                <InstagramIcon className="mx-4 w-10 h-10 fill-slate-600" />
-                <GithubIcon className="mx-4 w-10 h-10 fill-slate-600" />
-                <LinkedInIcon className="mx-4 w-10 h-10 fill-slate-600" />
+                <InstagramIcon className="w-10 h-10 mx-4 fill-slate-600" />
+                <GithubIcon className="w-10 h-10 mx-4 fill-slate-600" />
+                <LinkedInIcon className="w-10 h-10 mx-4 fill-slate-600" />
               </div>
             </div>
           </div>
           <div className="flex justify-center xs:mb-10 lg:w-1/2">
-            <img className="rounded-full w-1/2 lg:w-full" alt="Roman Raizen" src='/me.jpg' />
+            <img className="w-1/2 rounded-full lg:w-full" alt="Roman Raizen" src='/me.jpg' />
           </div>
         </div>
       </Blobs>
