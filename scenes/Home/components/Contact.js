a43c8c238f3f49900ff0e41803889fbf8036186e
Roman Raizen <raizen.me@icloud.com>
2022-01-09 15:50:38 +0700

    Form fixes

--- scenes/Home/components/Contact.js
+++ scenes/Home/components/Contact.js
@@ -1,51 +1,98 @@
 import React from 'react'
 import { useForm, ValidationError } from '@formspree/react'
 import Blobs from '@/components/Blobs'
+import CheckIcon from '@/components/CheckIcon'
+import SpinnerIcon from '@/components/SpinnerIcon'
+import classnames from 'classnames'
 
-const ContactForm = () => {
-  const [state, handleSubmit] = useForm("xwkyppeo")
-  if (state.succeeded) {
-      return <p>Thanks! I'll be back to you shortly</p>
-  }
+const Success = () => {
   return (
-      <Blobs width="w-1/2">
-        <div className="bg-white mb-20 p-12 rounded-xl m-10">
-          <form onSubmit={handleSubmit}>
-            <div>
-              <input
-                className="bg-slate-100 mb-10 rounded-md p-4 text-slate-600 focus:outline-none focus:border-purple-500 border-2 border-white w-full"
-                id="email"
-                type="email"
-                placeholder="Email"
-                name="email"
-              />
-              <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-              />
-            </div>
-            <div>
-              <textarea
-                rows="10"
-                id="message"
-                name="message"
-                placeholder="Your message"
-                className="bg-slate-100 mb-10 rounded-md p-4 text-slate-600 focus:outline-none focus:border-purple-500 border-2 border-white w-full"
-              />
-              <ValidationError
-                prefix="Message"
-                field="message"
-                errors={state.errors}
-              />
-            </div>
-            <button type="submit" disabled={state.submitting} className="px-6 py-4 whitespace-nowrap rounded-md text-white bg-gradient-to-tr bg-indigo-500 hover:transition-all transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-fuchsia-500 duration-300">
-              Send message
-            </button>
-          </form>
-        </div>
-      </Blobs>
+    <div className="flex flex-col items-center justify-center w-full h-full space-y-5">
+      <CheckIcon className="w-10 h-10 mx-4 stroke-lime-500" />
+      <div className="space-y-3 text-center">
+        <h1 className="text-3xl font-bold text-slate-600">Thank you!</h1>
+        <p className="text-lg text-slate-500">
+          I will get back to you as soon as possible.
+        </p>
+      </div>
+    </div>
   )
 }
 
+const ContactFormPresentation = ({ onSubmit, errors, isLoading, isSubmitted }) => {
+  return (
+    <Blobs width="w-1/2">
+      <div className="relative p-12 m-10 mb-20 bg-white rounded-xl">
+        { isSubmitted &&
+          <div className="absolute inset-0 z-10 bg-white">
+            <Success />
+          </div>
+        }
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              disabled={isSubmitted || isLoading}
+              className="w-full p-4 mb-10 border-2 border-white rounded-md bg-slate-100 text-slate-600 focus:outline-none focus:border-purple-500"
+              id="email"
+              type="email"
+              placeholder="Email"
+              required
+              name="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={errors}
+            />
+          </div>
+          <div>
+            <textarea
+              disabled={isSubmitted || isLoading}
+              rows="10"
+              id="message"
+              name="message"
+              required
+              placeholder="Your message"
+              className="w-full p-4 mb-10 border-2 border-white rounded-md bg-slate-100 text-slate-600 focus:outline-none focus:border-purple-500"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={errors}
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={isLoading || isSubmitted}
+            className={classnames("relative px-6 py-4 text-center text-white transition duration-300 ease-in-out delay-150 bg-indigo-500 rounded-md whitespace-nowrap bg-gradient-to-tr hover:transition-all", { "hover:-translate-y-1 hover:bg-fuchsia-500": !isLoading })}
+          >
+            { isLoading &&
+              <div className="absolute inset-0 flex items-center justify-center">
+                <SpinnerIcon className="w-6 h-6 fill-white animate-spin" />
+              </div>
+            }
+            <span className={classnames({ "opacity-0": isLoading })}>Send message</span>
+          </button>
+        </form>
+      </div>
+    </Blobs>
+)
+}
+
+
+const ContactForm = () => {
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORMSPREE)
+
+  return(
+    <ContactFormPresentation
+      isLoading={state.submitting}
+      isSubmitted={state.succeeded}
+      errors={state.errors}
+      onSubmit={handleSubmit}
+    />
+  )
+
+}
+
+export { ContactFormPresentation, Success }
 export default ContactForm
