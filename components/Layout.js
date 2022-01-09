0909737717315d3b02b6427484d6e80397850896
Roman Raizen <raizen.me@icloud.com>
2022-01-09 23:11:17 +0700

    Improves head

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,15 @@
+import Head from 'next/head'
+
 const Layout = ({ children }) => {
   return (
-    <main className="layout overflow-hidden">
+    <main className="overflow-hidden layout">
+      <Head>
+        <title>Roman Raizen - Web Creator - Personal Website</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       {children}
     </main>
   )
